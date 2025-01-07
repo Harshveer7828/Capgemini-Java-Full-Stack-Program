186ec995965accd4280918d1a3f529314818b2eb
harshveer7828 <harshveer7828@gmail.com>
2025-01-07 10:23:05 +0530

    Subtraction Functionality added

--- Calculator.java
+++ Calculator.java
@@ -2,10 +2,27 @@ import java.util.Scanner;
 public class Calculator{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the number on:-");
+		System.out.print("Enter the number one:-");
 		int a = sc.nextInt();
-		System.out.print("Enter the number on:-");
+		System.out.print("Enter the number two:-");
 		int b = sc.nextInt();
-		System.out.print("Addition is :- "+(a+b) );
+		System.out.println("Enter the operation\n1. Addition\n2. Subtraction");
+		int operation = sc.nextInt();
+		
+		switch(operation){
+			case 1:{
+				System.out.print("Sum of the number is :- "+ (a+b));
+				break;
+			}
+			case 2:{
+				System.out.print("Subtraction of the number is :- "+ (a-b));
+				break;
+			}
+			default : {
+				System.out.print("Ooops! Invalid input");
+				break;
+			}
+		}
+		
 	}
 }
